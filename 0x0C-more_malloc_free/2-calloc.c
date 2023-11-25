96180fea408ead876127adfca25ff829d4a1d478
Junior Mabunda <mjmabunda0@gmail.com>
2023-11-25 06:23:19 -0800

    Update to initialize array to zero

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,13 +12,19 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *arr;
+	unsigned int a;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
+	/* Allocate array memory, and check for failures */
 	arr = malloc(nmemb * size);
 	if (arr == NULL)
 		return (NULL);
+
+	/* Initialize memory to zero */
+	for (a = 0; a < nmemb; a++)
+		*((char *)arr + (a * size)) = 0;
 
 	return (arr);
 }
