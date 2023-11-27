e0eefc01b943bd0b886e2bfeeaf7fa3bcc31ae94
Junior Mabunda <mjmabunda0@gmail.com>
2023-11-27 06:35:10 -0800

    Add header file that defines the dog struct

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,4 @@
+# 0x0E. C - Structures, typedef
+This project explores `struct` and `typedef`.
+- a `struct` is away to group related variables
+- a `typedef` lets you define identifiers (like `int`, `float`, and `double`)
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * dog - defines a dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: defines a dog struct
+ */
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+}
+
+#endif
