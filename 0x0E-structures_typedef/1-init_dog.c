54fa68248c1fa7b17f720a40ce74930169d04fc6
Junior <36537112+ejmabunda@users.noreply.github.com>
2023-11-28 03:47:30 +0200

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,7 +11,11 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	// check if the pointers are not NULL before assigning values
+	if (d != NULL && name != NULL && owner != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
