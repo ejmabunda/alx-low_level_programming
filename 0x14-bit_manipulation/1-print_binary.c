66969ddd45ceeb7ff5d04e1689a5edbb57820167
Junior Mabunda <mjmabunda0@gmail.com>
2023-12-26 19:11:30 +0200

    Remove newline after each binary number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -14,7 +14,7 @@ void print_binary(unsigned long int n)
 
 	if (n == 0)
 	{
-		printf("0\n");
+		printf("0");
 		return;
 	}
 
@@ -33,6 +33,4 @@ void print_binary(unsigned long int n)
 		if (i == 0)
 			break;
 	}
-
-	putchar('\n');
 }
