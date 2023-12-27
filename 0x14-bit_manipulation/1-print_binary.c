e14627ae44f84ebed249fb51f8e7ecb39cded662
Junior Mabunda <mjmabunda0@gmail.com>
2023-12-27 09:21:29 +0200

    Use recursion to convert and print bits

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,28 +9,8 @@
  */
 void print_binary(unsigned long int n)
 {
-	int bin[64];
-	unsigned long int i;
+	if (n > 1)
+	    print_binary(n >> 1);
 
-	if (n == 0)
-	{
-		putchar('0');
-		return;
-	}
-
-	i = 0;
-	while (n > 0)
-	{
-		bin[i] = n & 1;
-		n = n >> 1;
-		i++;
-	}
-
-	while (1)
-	{
-		i--;
-		putchar(bin[i] + '0');
-		if (i == 0)
-			break;
-	}
+	putchar((n & 1) + '0');
 }
