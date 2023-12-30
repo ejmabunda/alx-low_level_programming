8e5dd9dbe1f7c3cd0acb83c8de3f4594913f998e
Junior Mabunda <mjmabunda0@gmail.com>
2023-12-30 18:41:24 +0200

    Add recursive function that checks if an integer is prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * is_prime_helper - entry point
+ * @n: integer to check
+ * @a: used to check factors
+ *
+ * Return: 1 if prime, 0 otherwise
+ */
+int is_prime_helper(int n, int a)
+{
+	/* base case */
+	if (n <= 1)
+		return (0);
+	else if (n == 2)
+		return (1);
+	else if (n % 2 == 0)
+		return (0);
+	else if (n % a == 0 && n != a)
+		return (0);
+	else if (n == a)
+		return (1);
+
+	/* recursive case */
+	return is_prime_helper(n, a + 1);
+}
+
+/**
+ * is_prime_number - checks if an integer is prime
+ * @n: integer to check
+ *
+ * Return: 1 if prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	return (is_prime_helper(n, 2));
+}
