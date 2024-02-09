14bb290575cc46e1755d092c1dc695612ea92c37
Junior <mjmabunda0@gmail.com>
2024-02-09 10:55:26 +0200

    Use open, close, write, and custom strlen

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <sys/stat.h>
-#include <unistd.h> /* access, F_OK */
+#include <fcntl.h>
+#include <unistd.h>
 
 #include "main.h"
 
@@ -15,29 +15,45 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	FILE *fptr;
+	ssize_t bytes_written; /* number of bytes written */
+	int fd; /* file descriptor */
 
 	if (filename == NULL)
-		return (-1);
+		return (-1); /* invalid filename */
 
-	/* check if file exists */
-	if (access(filename, F_OK) == -1)
+	/* open file for writing (append mode) */
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1); /* can't open file */
+
+	if (text_content != NULL)
 	{
-		/* file does not exist */
-		return (-1);
-	}
-	else
-	{
-		/* file exists */
-		if (text_content == NULL)
-			return (1);
-
-		fptr = fopen(filename, "a");
-		if (fptr == NULL)
-			return (-1);
-
-		fputs(text_content, fptr);
+		/* write text content to the file */
+		bytes_written = write(fd, text_content, _strlen(text_content));
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1); /* write error */
+		}
 	}
 
+	close(fd);
 	return (1);
+}
+
+/**
+ * _strlen - counts the number of characters in a string
+ * @str: string to check length of
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *str)
+{
+	int len;
+
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+
+	return (len);
 }
