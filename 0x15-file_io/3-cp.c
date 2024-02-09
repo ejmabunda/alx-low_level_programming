e47f4a334c9581d2437cbb2668ed96902a8d5de0
Junior <mjmabunda0@gmail.com>
2024-02-09 12:18:16 +0200

    Use open, close, read, write, and custom copy function

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/stat.h>
+#include <fcntl.h>
+
+#define BUFFER_SIZE 1024
 
 #include "main.h"
 
@@ -12,83 +14,81 @@
  * @argv: array of command-line arguments, including
  * the program's name.
  *
- * Return: Always 1 (success).
+ * Return: 0 on success,
+ * 97 on argument error,
+ * 98 on read error,
+ * 99 on write error, and
+ * 100 on close error
  */
 int main(int argc, char *argv[])
 {
-	FILE *from_ptr;
-	FILE *to_ptr;
-	char *file_from;
-	char *file_to;
-	char from_data[1024];
-	int to_exists;
+	int result;
 
-	/* number of arguments must be 2 */
-	if (argc -1 != 2)
+	if (argc != 3)
 	{
-		perror("Usage: cp file_from file_to\n");
-		exit(97);
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		return (97); /* argument error */
 	}
-	file_from = argv[1];
-	file_to = argv[2];
 
-	/* look for file_from */
-	if (access(file_from, F_OK) == -1)
+	result = copy_file(argv[1], argv[2]);
+	if (result == 98)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	else if (result == 99)
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
+
+	return (result);
+}
+
+/**
+ * copy_file - copies the content of a file to another file.
+ * @file_from: source file
+ * @file_to: destination file
+ *
+ * Return: 0 on success,
+ * 97 on argument error,
+ * 98 on read error,
+ * 99 on write error, and
+ * 100 on close error
+ */
+int copy_file(char *file_from, char *file_to)
+{
+	int fd_from; /* source file descriptor */
+	int fd_to; /* destination file descriptor */
+	ssize_t bytes_read, bytes_written; /* number of bytes read, written */
+	char buffer[BUFFER_SIZE]; /* buffer to store read content */
+
+	if (file_from == NULL || file_to == NULL)
+		return (97); /* invalid arguments */
+
+	/* open source file for reading */
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
+		return (98); /* can't open source file */
+
+	/* open destination file for writing, create if not found */
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd_to == -1)
+		return (99); /* can't create or write to destination file */
+
+	/* read from file_from and write to file_to */
+	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
-		/* file does not exit */
-		perror("Error: Can't read from file\n");
-		exit(98);
-	}
-	else
-	{
-		from_ptr = fopen(file_from, "r");
-		
-		/* Can't open file_from */
-		if (from_ptr == NULL)
+		bytes_written = write(fd_to, buffer, bytes_read);
+
+		if (bytes_written == -1)
 		{
-			perror("Error: Can't read from\n");
-			exit(99);
+			close(fd_from);
+			close(fd_to);
+			return (99); /* write error */
 		}
 	}
 
-	/* look for file_to */
-	if (access(file_to, F_OK) != -1)
-		to_exists = 1;
-	else
-		to_exists = 0;
-	
-	/* try to open */
-	to_ptr = fopen(file_to, "a");
+	/* clean up */
+	close(fd_from);
+	close(fd_to);
 
-/* Can't open file_to */
-	if (to_ptr == NULL)
-	{
-		perror("Error: Can't write to\n");
-		exit(99);
-	}
+	if (bytes_read == -1)
+		return (98); /* read error */
 
-	if (!to_exists)
-		chmod(file_to, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-		
-	/* copy data from original file to the copy */
-	while (fgets(from_data, 1024, from_ptr) != NULL)
-	{
-		fputs(from_data, to_ptr);
-	}
-	fputs("\n", to_ptr);
-
-	/* try to close the files */
-	if (fclose(from_ptr) != 0)
-	{
-		perror("Error: Can't close fd\n");
-		exit(100);
-	}
-	if (fclose(to_ptr) != 0)
-	{
-		perror("Error: Can't close fd\n");
-		exit(100);
-	}
-
-	/* Success */
-	return (1);
+	return (0); /* success */
 }
